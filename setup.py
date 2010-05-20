5bb6b4e6275d31e43a2eb2735b1999e4ae083d7e
tomer filiba <tomerfiliba@gmail.com>
2010-05-21 01:33:00 +0300

    srcgen

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='agnos_compiler',
-    version='0.1.0',
+    version='0.9',
     description='Agnos Compiler library',
     author=['Tomer Filiba'],
     author_email=['tomerfiliba@gmail.com'],
