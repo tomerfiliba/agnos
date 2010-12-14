73294dd2b58a6b9d2253ab2f3e9818d916208962
Tomer Filiba <tomerf@il.ibm.com>
2010-12-14 22:00:16 +0200

    re-add upload-to-sourceforge

---
+++ site/upload-to-sourceforge.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+make html
+if [ $? -ne 0 ]; then
+    echo "--make html failed--"
+    exit 1
+fi
+
+
+#rsync -r -v _build/html/ gangesmaster,agnos@shell.sourceforge.net:/home/groups/a/ag/agnos/htdocs
+rsync -r -v _build/html/ gangesmaster,agnos@web.sourceforge.net:htdocs/
+
+if [ $? -ne 0 ]; then
+    echo "--rsync failed--"
+    exit 1
+fi
